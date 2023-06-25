d7942a3171a37900af6f34759a4f47d867892401
sumitsharma001 <sharmasumit2023@gmail.com>
2023-06-25 16:55:51 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region     = "us-east-1"
+  version = "~>5.0"
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-022e1a32d3f742bd8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "first-ec2-instance"
+  }
+}
+
+
